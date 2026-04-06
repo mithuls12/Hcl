d4fbd05055797b2def1c7e711a253fb74b7d0a6d
mithuls12 <smithul904@gmail.com>
2026-04-06 14:58:03 +0530

    fix: update variable n to 9 in Main.java to match expected input values

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int m = 9, n = 5;
+        int m = 9, n = 9;
         System.out.println("Before swapping: m = " + m + ", n = " + n);
 
         int temp = m;
