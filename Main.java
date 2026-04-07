21e66973d24a6c7d936e03f4cb50883f07830417
mithuls12 <smithul904@gmail.com>
2026-04-07 09:52:55 +0530

    refactor: replace swap logic with hello world print statement

--- Main.java
+++ Main.java
@@ -1,13 +1,7 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        int m = 9, n = 9;
-        System.out.println("Before swapping: m = " + m + ", n = " + n);
-
-        int temp = m;
-        m = n;
-        n = temp;
-
-        System.out.println("After swapping: m = " + m + ", n = " + n);
+        System.out.println("Hello World");
     }
 }
